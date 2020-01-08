6386c807c62cafb41adbe643d9eb5c510979156d
Nicolae Mihalache <xpromache@gmail.com>
2020-01-08 09:39:28 +0100

    fix for creation of invalid partitions

--- yamcs-core/src/main/java/org/yamcs/yarch/TimePartitionSchema.java
+++ yamcs-core/src/main/java/org/yamcs/yarch/TimePartitionSchema.java
@@ -9,19 +9,21 @@ import org.yamcs.utils.TimeEncoding;
 import org.yamcs.utils.TaiUtcConverter.DateTimeComponents;
 
 /**
- * Implements different schemes for partitioning by time. 
+ * Implements different schemes for partitioning by time.
  * It gives back a partition start/end and a directory where data shall be stored.
- *  
+ * 
  * Currently the following are implemented:
- *   YYYY/DOY
- *   YYYY/MM
+ * YYYY/DOY
+ * YYYY/MM
+ * 
  * @author nm
  *
  */
 public abstract class TimePartitionSchema {
     /**
      * returns the directory where this instant shall be written.
-     * This is likely to be expensive operation - since instant has to be converted into a calendar taking into accounts leap seconds and all the rest.
+     * This is likely to be expensive operation - since instant has to be converted into a calendar taking into accounts
+     * leap seconds and all the rest.
      *
      * @param instant
      */
@@ -39,34 +41,34 @@ public abstract class TimePartitionSchema {
 
     static public TimePartitionSchema getInstance(String schema) {
         TimePartitionSchema tps;
-        if("YYYY/DOY".equalsIgnoreCase(schema)) {
+        if ("YYYY/DOY".equalsIgnoreCase(schema)) {
             tps = new YYYYDOY();
         } else if ("YYYY/MM".equalsIgnoreCase(schema)) {
             tps = new YYYYMM();
         } else if ("YYYY".equalsIgnoreCase(schema)) {
             tps = new YYYY();
         } else {
-            throw new IllegalArgumentException("Invalid time partitioning schema '"+schema+"'. Supported schemas are: YYYY/DOY, YYYY/MM and YYYY");
+            throw new IllegalArgumentException("Invalid time partitioning schema '" + schema
+                    + "'. Supported schemas are: YYYY/DOY, YYYY/MM and YYYY");
         }
 
         tps.name = schema;
         return tps;
     }
 
-
-
     static class YYYYDOY extends TimePartitionSchema {
         Pattern p = Pattern.compile("(\\d{4,})/(\\d{3})");
+
         @Override
         public TimePartitionInfo getPartitionInfo(long instant) {
-            DateTimeComponents dtc =TimeEncoding.toUtc(instant);
+            DateTimeComponents dtc = TimeEncoding.toUtc(instant);
             return getPartitionInfo(dtc.year, dtc.doy);
         }
 
         @Override
         public TimePartitionInfo parseDir(String dir) {
             Matcher m = p.matcher(dir);
-            if(m.matches()) {
+            if (m.matches()) {
                 int year = Integer.parseInt(m.group(1));
                 int doy = Integer.parseInt(m.group(2));
                 return getPartitionInfo(year, doy);
@@ -92,16 +94,17 @@ public abstract class TimePartitionSchema {
 
     static class YYYYMM extends TimePartitionSchema {
         Pattern p = Pattern.compile("(\\d{4,})/(\\d{2})");
+
         @Override
         public TimePartitionInfo getPartitionInfo(long instant) {
-            DateTimeComponents dtc =TimeEncoding.toUtc(instant);
+            DateTimeComponents dtc = TimeEncoding.toUtc(instant);
             return getPartitionInfo(dtc.year, dtc.month);
         }
 
         @Override
         public TimePartitionInfo parseDir(String dir) {
             Matcher m = p.matcher(dir);
-            if(m.matches()) {
+            if (m.matches()) {
                 int year = Integer.parseInt(m.group(1));
                 int month = Integer.parseInt(m.group(2));
                 return getPartitionInfo(year, month);
@@ -111,11 +114,11 @@ public abstract class TimePartitionSchema {
         }
 
         private TimePartitionInfo getPartitionInfo(int year, int month) {
-            TimePartitionInfo pinfo=new TimePartitionInfo();
+            TimePartitionInfo pinfo = new TimePartitionInfo();
             Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
             cal.clear();
             cal.set(Calendar.YEAR, year);
-            cal.set(Calendar.MONTH, month-1);
+            cal.set(Calendar.MONTH, month - 1);
             cal.set(Calendar.HOUR, 0);
 
             pinfo.setStart(TimeEncoding.fromCalendar(cal));
@@ -128,16 +131,17 @@ public abstract class TimePartitionSchema {
 
     static class YYYY extends TimePartitionSchema {
         Pattern p = Pattern.compile("(\\d{4,})");
+
         @Override
         public TimePartitionInfo getPartitionInfo(long instant) {
-            DateTimeComponents dtc =TimeEncoding.toUtc(instant);
+            DateTimeComponents dtc = TimeEncoding.toUtc(instant);
             return getPartitionInfo(dtc.year);
         }
 
         @Override
         public TimePartitionInfo parseDir(String dir) {
             Matcher m = p.matcher(dir);
-            if(m.matches()) {
+            if (m.matches()) {
                 int year = Integer.parseInt(m.group(1));
                 return getPartitionInfo(year);
             } else {
@@ -146,7 +150,7 @@ public abstract class TimePartitionSchema {
         }
 
         private TimePartitionInfo getPartitionInfo(int year) {
-            TimePartitionInfo pinfo=new TimePartitionInfo();
+            TimePartitionInfo pinfo = new TimePartitionInfo();
             Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
             cal.clear();
             cal.set(Calendar.YEAR, year);
@@ -157,11 +161,10 @@ public abstract class TimePartitionSchema {
             return pinfo;
         }
     }
-    
-    
 
     /**
      * name for the partitioning schema (YYYY/DOY, YYYY/MM, etc)
+     * 
      * @return the name of the partitioning schema
      */
     public String getName() {
